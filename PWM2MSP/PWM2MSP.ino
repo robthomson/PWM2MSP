fd8973cdd414acd09c50fd737475579112727d1c
Rob Thomson <rob@marotori.com>
2021-02-01 14:25:10 +0000

    voltage
    Extended the system slightly to allow 'voltage' of the flight pack to be detected.
    
    This is a little 'odd' in implimentation.
    
    What happens is you take a servo signal port.. and map it to a PWM input on the teensy.
    
    You can then on opentx/jeti etc send a value by telemetry to the pwm channel.
    
    Essentially.. take the telemetry of the flight pack from your radio telemetry.. and send it back to the receiver as a value betteen -1000 & +1000.
    
    This is then read into the teensy on pin 10 as a pwm signal; and translated to the right voltage for display.
    
    Its dirty.. but effective.

--- PWM2MSP/PWM2MSP.ino
+++ PWM2MSP/PWM2MSP.ino
@@ -1,11 +1,16 @@
 
 #include <MSP.h>
 #include "flt_modes.h"
-
+#include "MSP_OSD.h"
+#include "OSD_positions_config.h"
 
 HardwareSerial &mspSerial = Serial3;    //pin 8 tx pin 7 rx on teensy 3.2
 byte PWM_PIN = 9;
+byte PWMVOLTAGE_PIN = 10; //read the pwm pin to deliver a 'pwm/telemetry voltage scale to the dji unit'
 int pwm_value;
+double voltage_value;
+int max_voltage = 5;  //5 volts
+uint8_t vbat = 0;
 
 MSP msp;
 
@@ -25,12 +30,16 @@ void setup()
     mspSerial.begin(115200);
     msp.begin(mspSerial);
     pinMode(PWM_PIN, INPUT);
+    pinMode(PWMVOLTAGE_PIN, INPUT);
 }
 
 void loop()
 {
 
     pwm_value = pulseIn(PWM_PIN, HIGH);
+    voltage_value = pulseIn(PWMVOLTAGE_PIN , HIGH);
+
+  Serial.println(voltage_value );
     
     //send MSP data
     uint32_t currentMillis_MSP = millis();
@@ -43,7 +52,7 @@ void loop()
     
 }
 
-
+/*
 // MSP_STATUS reply customized for BF/DJI
 struct msp_status_BF_t {
   uint16_t cycleTime;
@@ -60,7 +69,7 @@ struct msp_status_BF_t {
   uint8_t  rebootRequired;
 } __attribute__ ((packed));
 
-
+*/
 
 void set_flight_mode_flags()
 {
@@ -75,6 +84,8 @@ void set_flight_mode_flags()
 }
 
 msp_status_BF_t status_BF = {0};
+msp_analog_t analog = {0};
+msp_battery_state_t battery_state = {0};
 
 void send_msp_to_airunit()
 {
@@ -82,4 +93,99 @@ void send_msp_to_airunit()
     status_BF.flightModeFlags = flightModeFlags;
     msp.send(MSP_STATUS, &status_BF, sizeof(status_BF));
 
+    vbat=map(voltage_value,1000,2000,0,max_voltage) * 10;
+
+    //MSP_ANALOG
+    analog.vbat = vbat;
+
+    msp.send(MSP_ANALOG, &analog, sizeof(analog));
+
+    
+    send_osd_config();
+
+}
+
+
+msp_osd_config_t msp_osd_config = {0};
+
+void send_osd_config()
+{
+  
+#ifdef IMPERIAL_UNITS
+    msp_osd_config.units = 0;
+#else
+    msp_osd_config.units = 1;
+#endif
+
+    msp_osd_config.osd_item_count = 1;
+    msp_osd_config.osd_stat_count = 24;
+    msp_osd_config.osd_timer_count = 2;
+    msp_osd_config.osd_warning_count = 16;              // 16
+    msp_osd_config.osd_profile_count = 1;              // 1
+    msp_osd_config.osdprofileindex = 1;                // 1
+    msp_osd_config.overlay_radio_mode = 0;             //  0
+
+    
+    msp_osd_config.osd_main_batt_voltage_pos = osd_main_batt_voltage_pos;
+   
+    /*
+ msp_osd_config.osd_rssi_value_pos = osd_rssi_value_pos; 
+    msp_osd_config.osd_crosshairs_pos = osd_crosshairs_pos;
+    msp_osd_config.osd_artificial_horizon_pos = osd_artificial_horizon_pos;
+    msp_osd_config.osd_horizon_sidebars_pos = osd_horizon_sidebars_pos;
+    msp_osd_config.osd_item_timer_1_pos = osd_item_timer_1_pos;
+    msp_osd_config.osd_item_timer_2_pos = osd_item_timer_2_pos;
+    msp_osd_config.osd_flymode_pos = osd_flymode_pos;
+    msp_osd_config.osd_craft_name_pos = osd_craft_name_pos;
+    msp_osd_config.osd_throttle_pos_pos = osd_throttle_pos_pos;
+    msp_osd_config.osd_vtx_channel_pos = osd_vtx_channel_pos;
+    msp_osd_config.osd_current_draw_pos = osd_current_draw_pos;
+    msp_osd_config.osd_mah_drawn_pos = osd_mah_drawn_pos;
+    msp_osd_config.osd_gps_speed_pos = osd_gps_speed_pos;
+    msp_osd_config.osd_gps_sats_pos = osd_gps_sats_pos;
+    msp_osd_config.osd_altitude_pos = osd_altitude_pos;
+    msp_osd_config.osd_roll_pids_pos = osd_roll_pids_pos;
+    msp_osd_config.osd_pitch_pids_pos = osd_pitch_pids_pos;
+    msp_osd_config.osd_yaw_pids_pos = osd_yaw_pids_pos;
+    msp_osd_config.osd_power_pos = osd_power_pos;
+    msp_osd_config.osd_pidrate_profile_pos = osd_pidrate_profile_pos;
+    msp_osd_config.osd_warnings_pos = osd_warnings_pos;
+    msp_osd_config.osd_avg_cell_voltage_pos = osd_avg_cell_voltage_pos;
+    msp_osd_config.osd_gps_lon_pos = osd_gps_lon_pos;
+    msp_osd_config.osd_gps_lat_pos = osd_gps_lat_pos;
+    msp_osd_config.osd_debug_pos = osd_debug_pos;
+    msp_osd_config.osd_pitch_angle_pos = osd_pitch_angle_pos;
+    msp_osd_config.osd_roll_angle_pos = osd_roll_angle_pos;
+    msp_osd_config.osd_main_batt_usage_pos = osd_main_batt_usage_pos;
+    msp_osd_config.osd_disarmed_pos = osd_disarmed_pos;
+    msp_osd_config.osd_home_dir_pos = osd_home_dir_pos;
+    msp_osd_config.osd_home_dist_pos = osd_home_dist_pos;
+    msp_osd_config.osd_numerical_heading_pos = osd_numerical_heading_pos;
+    msp_osd_config.osd_numerical_vario_pos = osd_numerical_vario_pos;
+    msp_osd_config.osd_compass_bar_pos = osd_compass_bar_pos;
+    msp_osd_config.osd_esc_tmp_pos = osd_esc_tmp_pos;
+    msp_osd_config.osd_esc_rpm_pos = osd_esc_rpm_pos;
+    msp_osd_config.osd_remaining_time_estimate_pos = osd_remaining_time_estimate_pos;
+    msp_osd_config.osd_rtc_datetime_pos = osd_rtc_datetime_pos;
+    msp_osd_config.osd_adjustment_range_pos = osd_adjustment_range_pos;
+    msp_osd_config.osd_core_temperature_pos = osd_core_temperature_pos;
+    msp_osd_config.osd_anti_gravity_pos = osd_anti_gravity_pos;
+    msp_osd_config.osd_g_force_pos = osd_g_force_pos;
+    msp_osd_config.osd_motor_diag_pos = osd_motor_diag_pos;
+    msp_osd_config.osd_log_status_pos = osd_log_status_pos;
+    msp_osd_config.osd_flip_arrow_pos = osd_flip_arrow_pos;
+    msp_osd_config.osd_link_quality_pos = osd_link_quality_pos;
+    msp_osd_config.osd_flight_dist_pos = osd_flight_dist_pos;
+    msp_osd_config.osd_stick_overlay_left_pos = osd_stick_overlay_left_pos;
+    msp_osd_config.osd_stick_overlay_right_pos = osd_stick_overlay_right_pos;
+    msp_osd_config.osd_display_name_pos = osd_display_name_pos;
+    msp_osd_config.osd_esc_rpm_freq_pos = osd_esc_rpm_freq_pos;
+    msp_osd_config.osd_rate_profile_name_pos = osd_rate_profile_name_pos;
+    msp_osd_config.osd_pid_profile_name_pos = osd_pid_profile_name_pos;
+    msp_osd_config.osd_profile_name_pos = osd_profile_name_pos;
+    msp_osd_config.osd_rssi_dbm_value_pos = osd_rssi_dbm_value_pos;
+    msp_osd_config.osd_rc_channels_pos = osd_rc_channels_pos;
+  */
+
+    msp.send(MSP_OSD_CONFIG, &msp_osd_config, sizeof(msp_osd_config));
 }
